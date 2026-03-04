ca6f0e62b80898f4beb7cd08827848481b07dff4
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 14:43:14 +0530

    initial commit

---
+++ src/main/java/com/medisphere/telemedicine/MedisphereTelemedicineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.telemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereTelemedicineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereTelemedicineServiceApplication.class, args);
+	}
+
+}
