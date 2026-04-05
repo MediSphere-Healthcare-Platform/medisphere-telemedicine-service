bc6085a6761f098cbe45956092575a6ea3875c18
AroshXR <aroshanasandeep454@gmail.com>
2026-04-05 22:03:51 +0530

    application.properties

--- src/main/java/com/medisphere/telemedicine/MedisphereTelemedicineServiceApplication.java
+++ src/main/java/com/medisphere/telemedicine/MedisphereTelemedicineServiceApplication.java
@@ -1,9 +1,10 @@
 package com.medisphere.telemedicine;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedisphereTelemedicineServiceApplication {
 
 	public static void main(String[] args) {
